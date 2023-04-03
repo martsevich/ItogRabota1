210409cb6c6f4980e6f27f1d315c37eb5a120a62
Dzmitry Martsevich <dipups@tut.by>
2023-04-03 21:29:58 +0300

    создал массивы и метод SaveInArray2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача :
+// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+// При решение не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами
+
+string[] array1 = new string[] { "hello","2","world",":-)","7t>" };
+string[] array2 = new string[array1.Length];
+
+void SaveInArray2(string[] array1, string[] array2)
+{
+    int j = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[j] = array1[i];
+        }
+        j++;
+    }
+}
+
